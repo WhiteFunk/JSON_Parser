36a95079eeee7921d15b5fe1b6482c9545fc01f1
Ilya <odinets.ilya.careers@gmail.com>
2022-09-29 22:08:59 +0300

    Update JSON_Parser.cpp

--- JSON_Parser/JSON_Parser.cpp
+++ JSON_Parser/JSON_Parser.cpp
@@ -1,4 +1,25 @@
-﻿#include <map>
+/******************************************************************************************/
+/*                                                                                        */
+/*    Simple XML parser on C++                                                            */
+/*                                                                                        */
+/*    ----------------------------------------------------------                          */
+/*                                                                                        */
+/*                                                                                        */
+/*    This program is free software: you can redistribute it and/or modify                */
+/*    it under the terms of the GNU General Public License as published by                */
+/*    the Free Software Foundation, either version 3 of the License, or                   */
+/*    (at your option) any later version.                                                 */
+/*                                                                                        */
+/*                                                                                        */
+/*                                                                                        */
+/*    Contact:   odinets.ilya.careers@gmail.com                                           */
+/*               www.linkedin.com/in/ilya-odinets-238ba124a                               */
+/*                                                                                        */
+/*                                                                                        */
+/******************************************************************************************/﻿
+﻿
+
+#include <map>
 #include <vector>
 #include <string>
 #include <initializer_list>
